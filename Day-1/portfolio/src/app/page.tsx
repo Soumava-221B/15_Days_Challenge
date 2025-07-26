2ef353708c221af7dcd8fb9b31a7de45c41cda48
Soumava Das <soumavadas06@gmail.com>
2025-07-26 19:47:51 +0530

    Featured Project Section

--- Day-1/portfolio/src/app/page.tsx
+++ Day-1/portfolio/src/app/page.tsx
@@ -1,6 +1,11 @@
 import Hero from './components/Hero';
+import Projects from './components/Projects';
+
 export default function Home() {
   return (
-    <><Hero/></>
+    <>
+    <Hero/>
+    <Projects/>
+    </>
   );
 }
