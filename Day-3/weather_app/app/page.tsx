ed8cbf2ccdf4fd61f3d98ec778870fdc49a3c5cb
Soumava Das <soumavadas06@gmail.com>
2025-07-28 08:46:33 +0530

    basic setup completed

---
+++ Day-3/weather_app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-sky-400 to-blue-500 p-4 flex items-center justify-center">
+      <div className="w-full max-w-md space-y-4">
+        <form ></form>
+      </div>
+    </div>
+  );
+}
