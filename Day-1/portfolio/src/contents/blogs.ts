963bcb267e3f042040a00580db43ce1daf6659ff
Soumava Das <soumavadas06@gmail.com>
2025-07-28 07:47:36 +0530

    blog page added

--- Day-1/portfolio/src/contents/blogs.ts
+++ Day-1/portfolio/src/contents/blogs.ts
@@ -8,4 +8,11 @@ export const blogs: Blog[] = [
         readTime: '5 min read',
         slug: 'https://medium.com/@soumavadas22/api-102-building-workflows-with-apis-in-postman-8549fd7fbdd8',
       },
+      {
+        title: 'Behind the scenes: Creating my personal Portfolio Project',
+        excerpt: 'Portfolio Project',
+        date: '2025-07-27',
+        readTime: '3 min read',
+        slug: 'https://soumavadas.hashnode.dev/behind-the-scenes-creating-my-personal-portfolio-project',
+      },
   ];
