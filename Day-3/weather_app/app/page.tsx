765adfd1e61f3acfb31cae0bcca0b676179f9d03
Soumava Das <soumavadas06@gmail.com>
2025-07-28 10:25:43 +0530

    fetching data from API key

--- Day-3/weather_app/app/page.tsx
+++ Day-3/weather_app/app/page.tsx
@@ -1,10 +1,43 @@
-import Image from "next/image";
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+import { getWeatherData } from "./actions";
+
+function SubmitButton() {
+  return (
+    <Button type="submit">
+      <Search className="w-4 h-4" />
+    </Button>
+  )
+}
 
 export default function Home() {
+
+  const handleSeach = async(formData: FormData) => {
+    const city = formData.get("city") as string;
+    const result = await getWeatherData(city);
+    if (result && "data" in result) {
+      console.log(result.data);
+    } else {
+      console.log("No data found");
+    }
+  }
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-sky-400 to-blue-500 p-4 flex items-center justify-center">
       <div className="w-full max-w-md space-y-4">
-        <form ></form>
+        <form action={handleSeach} className="flex gap-2">
+          <Input
+          name="city"
+          type="text"
+          placeholder="Enter city name..."
+          className="bg-white/90"
+          required
+          />
+          <SubmitButton />
+        </form>
       </div>
     </div>
   );
