6152ca71cfe9475eb260aa3c2084e839f649f83e
Soumava Das <soumavadas06@gmail.com>
2025-08-03 20:35:20 +0530

    start timer function

--- Day-8/pomodoro/app/components/timer.tsx
+++ Day-8/pomodoro/app/components/timer.tsx
@@ -1,4 +1,17 @@
+"use client";
+
+import useTimer from "../hooks/useTimer";
+
 export default function Timer() {
+  const { mode, timeLeft, toggleTimer, isActive } = useTimer();
+
+  // Format time as MM:SS
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+  const formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+
   return (
     <div className="max-w-sm bg-white shadow-xl w-full p-6 rounded-xl ">
       <div className="flex items-center justify-between mb-4">
@@ -6,14 +19,24 @@ export default function Timer() {
         <div className="text-sm text-gray-500">Sessions: 1</div>
       </div>
 
-      <div className="text-center text-6xl font-bold mb-6 text-red-500">25:00</div>
-
-      <div className="flex justify-center space-x-4 mb-4">
-        <button className="bg-yellow-500 hover:bg-yellow-600 px-5 py-2 rounded-lg text-white">Pause</button>
-        <button className="bg-gray-200 hover:bg-gray-300 px-5 py-2 rounded-lg">Reset</button>
+      <div className="text-center text-6xl font-bold mb-6 text-red-500">
+        {formattedTime}
       </div>
 
-      <button className="py-2 w-full bg-gray-100 hover:bg-gray-200 rounded-lg text-gray-700">Switch to Break</button>
+      <div className="flex justify-center space-x-4 mb-4">
+        <button 
+        onClick={toggleTimer}
+        className="bg-yellow-500 hover:bg-yellow-600 px-5 py-2 rounded-lg text-white">
+          {isActive ? "Pause" : "Start"}
+        </button>
+        <button className="bg-gray-200 hover:bg-gray-300 px-5 py-2 rounded-lg">
+          Reset
+        </button>
+      </div>
+
+      <button className="py-2 w-full bg-gray-100 hover:bg-gray-200 rounded-lg text-gray-700">
+        Switch to Break
+      </button>
     </div>
   );
 }
---
+++ Day-8/pomodoro/app/hooks/useTimer.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect, useState } from "react";
+type TimerMode = "focus" | "break";
+
+const focusTime = 25;
+const breakTime = 5;
+
+export default function useTimer() {
+  const [mode, setMode] = useState<TimerMode>("focus");
+  const [timeLeft, setTimeLeft] = useState(focusTime * 60);
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  const toggleTimer = () => {
+    setIsActive(prev => !prev);
+  }
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+
+    if(isActive) {
+      interval = setInterval(() => {
+      setTimeLeft((prev) => {
+        return prev - 1;
+      });
+    }, 1000);
+    } else if(interval) {
+      clearInterval(interval);
+    }
+
+    return () => {
+      if(interval) clearInterval(interval);
+    };
+  }, [isActive]);
+
+  return {
+    mode,
+    timeLeft,
+    toggleTimer,
+    isActive
+  };
+}
