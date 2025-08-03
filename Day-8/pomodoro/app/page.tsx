596adc282989f3caf97d65523ec45d13b786a6c9
Soumava Das <soumavadas06@gmail.com>
2025-08-03 17:57:26 +0530

    baisc UI

---
+++ Day-8/pomodoro/app/page.tsx
@@ -0,0 +1,9 @@
+import Timer from './components/timer';
+export default function Home() {
+  return (
+    <div className="bg-gray-50 min-h-screen flex flex-col items-center justify-center p-4">
+      <h1 className='text-3xl font-bold mb-8 text-gray-800'>Simple Pomodoro</h1>
+      <Timer/>
+    </div>
+  );
+}
