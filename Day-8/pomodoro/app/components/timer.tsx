ccae64360b1a6098e1016b144cdfea129dbce1b9
Soumava Das <soumavadas06@gmail.com>
2025-08-03 22:46:40 +0530

    pomodoro complete

--- Day-8/pomodoro/app/components/timer.tsx
+++ Day-8/pomodoro/app/components/timer.tsx
@@ -3,7 +3,9 @@
 import useTimer from "../hooks/useTimer";
 
 export default function Timer() {
-  const { mode, timeLeft, toggleTimer, isActive } = useTimer();
+  const { mode, timeLeft, toggleTimer, isActive, resetTimer, switchMode, sessions } = useTimer();
+
+  console.log(mode);
 
   // Format time as MM:SS
   const minutes = Math.floor(timeLeft / 60);
@@ -15,27 +17,31 @@ export default function Timer() {
   return (
     <div className="max-w-sm bg-white shadow-xl w-full p-6 rounded-xl ">
       <div className="flex items-center justify-between mb-4">
-        <h2 className="text-xl font-medium capitalize">Focus time</h2>
-        <div className="text-sm text-gray-500">Sessions: 1</div>
+        <h2 className="text-xl font-medium capitalize">{mode === "focus" ? "🎯 Focus Time" : "☕️ Break Time"}</h2>
+        <div className="text-sm text-gray-500">Sessions: {sessions}</div>
       </div>
 
-      <div className="text-center text-6xl font-bold mb-6 text-red-500">
+      <div className={`text-center text-6xl font-bold mb-6 ${mode === "focus" ? "text-red-500" : "text-green-500"} `}>
         {formattedTime}
       </div>
 
       <div className="flex justify-center space-x-4 mb-4">
         <button 
         onClick={toggleTimer}
-        className="bg-yellow-500 hover:bg-yellow-600 px-5 py-2 rounded-lg text-white">
+        className={`px-5 py-2 rounded-lg font-medium ${isActive ? "bg-yellow-500 hover:bg-yellow-600 text-white" : "bg-blue-500 hover:bg-blue-600 text-white"} `}>
           {isActive ? "Pause" : "Start"}
         </button>
-        <button className="bg-gray-200 hover:bg-gray-300 px-5 py-2 rounded-lg">
-          Reset
+        <button 
+        onClick={resetTimer}
+        className="bg-gray-200 hover:bg-gray-300 px-5 py-2 rounded-lg">
+            Reset
         </button>
       </div>
 
-      <button className="py-2 w-full bg-gray-100 hover:bg-gray-200 rounded-lg text-gray-700">
-        Switch to Break
+      <button 
+      onClick={switchMode}
+      className="py-2 w-full bg-gray-100 hover:bg-gray-200 rounded-lg text-gray-700">
+        Switch to {mode === "focus" ? "Break" : "Focus"}
       </button>
     </div>
   );
--- Day-8/pomodoro/app/hooks/useTimer.tsx
+++ Day-8/pomodoro/app/hooks/useTimer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 type TimerMode = "focus" | "break";
 
 const focusTime = 25;
@@ -10,10 +10,32 @@ export default function useTimer() {
   const [mode, setMode] = useState<TimerMode>("focus");
   const [timeLeft, setTimeLeft] = useState(focusTime * 60);
   const [isActive, setIsActive] = useState<boolean>(false);
+  const [sessions, setSessions] = useState(0);
+
+  const getDuration = useCallback((timerMode: TimerMode) => {
+    return timerMode === "focus" ? focusTime * 60 : breakTime * 60;
+  }, []);
+
+  const resetTimer = () => {
+    setIsActive(false);
+    setTimeLeft(getDuration(mode));
+  }
 
   const toggleTimer = () => {
     setIsActive(prev => !prev);
   }
+
+  const switchMode = useCallback(() => {
+    const newMode = mode === "focus" ? "break" : "focus";
+    setMode(newMode);
+    setTimeLeft(getDuration(newMode));
+    setIsActive(false);
+
+    // increment sessions
+    if (mode === "focus") {
+      setSessions((prev) => prev + 1);
+    }
+  }, [mode, getDuration]);
 
   useEffect(() => {
     let interval: NodeJS.Timeout | null = null;
@@ -21,7 +43,18 @@ export default function useTimer() {
     if(isActive) {
       interval = setInterval(() => {
       setTimeLeft((prev) => {
-        return prev - 1;
+        if (prev <= 1) {
+          // Time's up
+          clearInterval(interval!);
+
+          // play notifcation sound
+          try {
+            const audio = new Audio("/notification.mp3");
+            audio.play().catch((e) => console.log("Error playing sound", e));
+          } catch (error) {
+            console.log("Error playing sound", error);
+          }
+        } return prev - 1;
       });
     }, 1000);
     } else if(interval) {
@@ -33,10 +66,20 @@ export default function useTimer() {
     };
   }, [isActive]);
 
+  // Auto-switch mode when timer reaches 0
+  useEffect(() => {
+    if (timeLeft === 0) {
+      switchMode();
+    }
+  }, [timeLeft, switchMode]);
+
   return {
     mode,
     timeLeft,
     toggleTimer,
-    isActive
+    isActive,
+    resetTimer,
+    switchMode,
+    sessions
   };
 }
