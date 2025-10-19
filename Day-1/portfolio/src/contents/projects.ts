33d57658682fbf0cbd0347ecfb29e3239784c23d
Soumava Das <soumavadas06@gmail.com>
2025-10-19 12:19:11 +0530

    Updated the projects

--- Day-1/portfolio/src/contents/projects.ts
+++ Day-1/portfolio/src/contents/projects.ts
@@ -25,4 +25,28 @@ export const projects: Project[] = [
         demoLink: 'https://react-test-pocket-notes.vercel.app/',
         image: '/projects/PocketNotes.png',
       },
+      {
+        title: 'Weather App',
+        description: 'A real-time weather app that delivers accurate forecasts',
+        technologies: ['Next.js',  'OpenWeather API'],
+        githubLink: 'https://github.com/Soumava-221B/15_Days_Challenge/tree/main/Day-3/weather_app',
+        demoLink: 'https://weatherapp-orpin-three-42.vercel.app/',
+        image: '/projects/WeatherApp.png',
+      },
+      {
+        title: 'Memory Card Game',
+        description: 'A memory card game challenges players to find matching pairs by remembering the locations of cards.',
+        technologies: ['React.js',  'Typescript'],
+        githubLink: 'https://github.com/Soumava-221B/15_Days_Challenge/tree/main/Day-6/memory_game',
+        demoLink: 'https://memory-game-2025-ebon.vercel.app/',
+        image: '/projects/MemoryGame.png',
+      },
+      {
+        title: 'Random Quote Generator',
+        description: 'A quote generator app displays random inspirational or thought-provoking quotes at the tap of a button',
+        technologies: ['Next.js',  'Typescript'],
+        githubLink: 'https://github.com/Soumava-221B/15_Days_Challenge/tree/main/Day-5/quote_generator',
+        demoLink: 'https://quote-generator-2025-sooty.vercel.app/',
+        image: '/projects/QuoteGenerator.png',
+      },
   ];
